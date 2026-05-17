073f581a085f7d37e7ebc679be439a235f759d29
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-17 18:39:38 -0300

    chore: estrutura diretórios do projeto seguindo práticas do fsd

---
+++ cinedash/src/app/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+      <h1>CineDash</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
