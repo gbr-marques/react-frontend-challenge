57955910287493ba3732a4eb67969bc0ecb86aac
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-18 15:30:58 -0300

    feat: estrutura layout e rota de página de home

---
+++ cinedash/src/pages/home/ui/home-page.tsx
@@ -0,0 +1,22 @@
+const HomePage = () => {
+  return (
+    <>
+      <section className="min-h-[75dvh] bg-[#1D242A] p-4 py-8 flex flex-col">
+        <div className="text-white text-center leading-tight flex flex-col gap-4">
+          <h1 className="text-2xl">
+            Bem-vindo, curador! Pronto para descobrir novos filmes?
+          </h1>
+          <p>
+            Esta é a homepage do CineDash, uma plataforma de curadoria e
+            descoberta de filmes desenvolvida para auxiliar equipes de streaming
+            na análise, organização e seleção de títulos. Aqui você pode
+            explorar tendências, descobrir novos filmes e acompanhar informações
+            relevantes para a construção de catálogos cinematográficos.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HomePage;
--- cinedash/src/routes/home.tsx
+++ cinedash/src/routes/home.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+import HomePage from '../pages/home/ui/home-page'
 
 export const Route = createFileRoute('/home')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/home"!</div>
+  return <HomePage />
 }
