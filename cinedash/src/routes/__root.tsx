112a294517731c34cf80b19ff2eea14049401fcd
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-18 13:14:42 -0300

    chore: instala deps das libs lucide-react e router-vite-plugin

--- cinedash/src/routes/__root.tsx
+++ cinedash/src/routes/__root.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import Header from "../widgets/header";
+import Footer from "../widgets/footer";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -8,7 +10,9 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <React.Fragment>
+      <Header></Header>
       <Outlet />
+      <Footer></Footer>
     </React.Fragment>
   );
 }
