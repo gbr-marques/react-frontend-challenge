5bc4405234a00f64b64128c01c2095ba36b0c471
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-18 15:16:08 -0300

    feat: estrutura layout da oagina de login

---
+++ cinedash/src/pages/login/ui/login-page.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { Label } from "../../../components/ui/label";
+import { Input } from "../../../components/ui/input";
+import { Button } from "../../../components/ui/button";
+import { Eye, EyeOff } from "lucide-react";
+import logo from "@/shared/assets/cinedash_logo.png";
+import { Checkbox } from "../../../components/ui/checkbox";
+import { useNavigate } from "@tanstack/react-router";
+
+export function LoginPage() {
+  const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const authenticateUser = () => {
+    navigate({ to: "/home" });
+  };
+
+  return (
+    <section className="flex min-h-screen items-center justify-center p-4">
+      <div className="bg-[#1D242A] flex rounded-md shadow-lg min-h-150 overflow-hidden">
+        <div className="w-1/2 bg-red-400"></div>
+        <div className="flex flex-col items-center justify-center text-center gap-4 p-4 md:p-8 text-white leading-tight md:w-1/2">
+          <img src={logo} alt="Logo do CineDash" className="w-[60%] max-w-64" />
+          <h1 className="text-3xl font-bold">
+            A sua plataforma de curadoria e descoberta de filmes
+          </h1>
+          <p className="font-extralight">
+            Lorem ipsum dolor sit amet consectetur. Egestas sit felis aenean
+            tincidunt sem. Hendrerit consequat id diam quam dignissim
+            pellentesque quam ultrices sed.
+          </p>
+          <div className="flex flex-col gap-2 w-full">
+            <Label htmlFor="password-toggle">E-mail</Label>
+            <div className="relative">
+              <Input
+                className="bg-background h-12"
+                id="password-toggle"
+                placeholder="Digite seu e-mail"
+                type="text"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 w-full">
+            <Label htmlFor="password-toggle">Senha</Label>
+            <div className="relative">
+              <Input
+                className="bg-background h-12"
+                id="password-toggle"
+                placeholder="Digite sua senha"
+                type={showPassword ? "text" : "password"}
+              />
+              <Button
+                className="absolute top-0 right-0 h-full px-3 hover:bg-transparent"
+                onClick={() => setShowPassword(!showPassword)}
+                size="icon"
+                type="button"
+                variant="ghost"
+              >
+                {showPassword ? (
+                  <EyeOff className="h-4 w-4 text-muted-foreground" />
+                ) : (
+                  <Eye className="h-4 w-4 text-muted-foreground" />
+                )}
+              </Button>
+            </div>
+          </div>
+          <span className="w-full flex gap-2 text-sm items-center text-gray-400">
+            <Checkbox></Checkbox> Mantenha-me conectado{" "}
+          </span>
+          <Button
+            className="bg-[#F98635] w-full h-12"
+            onClick={authenticateUser}
+          >
+            Entrar
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+}
