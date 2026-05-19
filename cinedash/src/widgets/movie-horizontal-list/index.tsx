a3f6252c53aaba0c691a03d40d489ed04d7b01e6
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-19 20:37:39 -0300

    feat: adiciona hooks e implementa dados dinâmicos ao widget de lista horizontal e componente de movie card

--- cinedash/src/widgets/movie-horizontal-list/index.tsx
+++ cinedash/src/widgets/movie-horizontal-list/index.tsx
@@ -1,25 +1,35 @@
+import { useState } from "react";
 import { Button } from "../../components/ui/button";
+import { usePopularMovies } from "../../entities/movie/api/use-popular-movies";
 import MovieCard from "../../shared/ui/movie-card";
+import type { TMovie } from "../../entities/movie/model/types";
 
 const MovieHorizontalList = () => {
+  const { data, isLoading } = usePopularMovies();
+
+  if (isLoading) {
+    console.log("Carregando filmes...");
+  }
+
   return (
     <>
       <div className="flex flex-col gap-2 text-white">
         <div className="flex justify-between">
-            <h3 className="uppercase font-black text-xl md:text-2xl">Filmes populares</h3>
-            <Button variant="link" className="text-muted-foreground">Ver mais...</Button>
+          <h3 className="uppercase font-black text-xl md:text-2xl">
+            Filmes populares
+          </h3>
+          <Button variant="link" className="text-muted-foreground">
+            Ver mais...
+          </Button>
         </div>
-        <div className="flex gap-2 overflow-x-auto pb-4 h-60 md:h-80">
-            <MovieCard></MovieCard>
-            {/* <div className="h-42 min-w-30 rounded-sm bg-[#202020] border border-[#4D4D4D]"></div>
-            <div className="h-42 min-w-30 rounded-sm bg-[#202020] border border-[#4D4D4D]"></div>
-            <div className="h-42 min-w-30 rounded-sm bg-[#202020] border border-[#4D4D4D]"></div>
-            <div className="h-42 min-w-30 rounded-sm bg-[#202020] border border-[#4D4D4D]"></div>
-            <div className="h-42 min-w-30 rounded-sm bg-[#202020] border border-[#4D4D4D]"></div>
-            <div className="h-42 min-w-30 rounded-sm bg-[#202020] border border-[#4D4D4D]"></div>
-            <div className="h-42 min-w-30 rounded-sm bg-[#202020] border border-[#4D4D4D]"></div>
-            <div className="h-42 min-w-30 rounded-sm bg-[#202020] border border-[#4D4D4D]"></div>
-            <div className="h-42 min-w-30 rounded-sm bg-[#202020] border border-[#4D4D4D]"></div> */}
+        <div className="flex gap-2 md:gap-4 overflow-x-scroll pb-4 h-60 md:h-80 w-full">
+          {isLoading ? (
+            <p>Carregando filmes...</p>
+          ) : (
+            data?.map((movie: TMovie) => (
+              <MovieCard movie={movie} key={movie.id}></MovieCard>
+            ))
+          )}
         </div>
       </div>
     </>
