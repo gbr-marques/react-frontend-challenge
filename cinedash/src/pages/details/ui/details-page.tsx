ffd8cdb555b91ac8ca970016c7e0ace146fb4959
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-19 22:57:23 -0300

    feat: substitui dados mockados por dados dinamincos

--- cinedash/src/pages/details/ui/details-page.tsx
+++ cinedash/src/pages/details/ui/details-page.tsx
@@ -9,6 +9,8 @@ import { Badge } from "../../../components/ui/badge";
 import { Button } from "../../../components/ui/button";
 import { Route } from "../../../routes/details/$id";
 import { useMovieDetails } from "../../../entities/movie/api/use-movie-details";
+import moment from "moment";
+import { CastList } from "../../../widgets/cast-list";
 
 export function DetailsPage() {
   const { id } = Route.useParams();
@@ -19,64 +21,58 @@ export function DetailsPage() {
 
   return (
     <>
-      <section className="min-h-[75dvh] bg-[#1D242A] p-4 md:p-24 py-8 flex flex-col gap-8 items-center">
+      <section
+        className="min-h-[75dvh] p-4 md:p-24 py-8 flex flex-col gap-8 items-center bg-cover bg-center"
+        style={{
+          backgroundImage: `
+          linear-gradient(
+            to bottom,
+            rgba(29, 36, 42, 0.70) 0px,
+            #1D242A ${window.innerWidth < 768 ? "250px" : "400px"}
+          ),
+          url(https://image.tmdb.org/t/p/original${data?.backdrop_path})
+        `,
+        }}
+      >
         <div className="flex flex-col md:flex-row gap-4 md:gap-8 items-center">
           <img
-            src="https://posterhouse.org/wp-content/uploads/2021/05/moonlight_0.jpg"
+            src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
             alt="Poster do filme"
-            className="w-40 max-w-100 md:w-2/5 h-full rounded-sm border border-[#4D4D4D] shadow-lg"
+            className="w-40 max-w-100 md:w-2/5 h-full rounded-sm border border-[#1d242aea] shadow-lg"
           />
           <div className="flex flex-col gap-2 max-w-150">
             <div className="flex items-end gap-2">
               <h1 className="text-white text-3xl">{data?.title}</h1>
-              <span className="text-gray-400 text-xs">{} - 2h12m</span>
+              <span className="text-gray-400 text-md font-extralight">
+                {moment(data?.release_date).year()}
+              </span>
             </div>
             <div className="flex items-end gap-2">
               <span className="flex">
-                <Star className="text-yellow-300 fill-yellow-300 h-5"></Star>
-                <Star className="text-yellow-300 fill-yellow-300 h-5"></Star>
-                <Star className="text-yellow-300 fill-yellow-300 h-5"></Star>
-                <Star className="text-yellow-300 fill-yellow-300 h-5"></Star>
-                <Star className="text-yellow-300 fill-yellow-300 h-5"></Star>
-                <Star className="text-yellow-300 fill-yellow-300 h-5"></Star>
-                <Star className="text-yellow-300 fill-yellow-300 h-5"></Star>
-                <Star className="text-yellow-300 fill-yellow-300 h-5"></Star>
+                {Array.from({
+                  length: Math.round(Number(data?.vote_average)),
+                }).map(() => (
+                  <Star className="text-yellow-300 fill-yellow-300 h-5"></Star>
+                ))}
               </span>
-              <span className="text-sm text-gray-400 leading-tight">8.1</span>
+              <span className="text-sm text-gray-400 leading-tight">
+                {data?.vote_average.toString().slice(0, 3)}
+              </span>
             </div>
             <span className="text-gray-400 font-extralight text-xl">
-              A long time ago in a galaxy far, far away...
+              {data?.tagline}
             </span>
-            <span className="text-xs font-extralight text-gray-400">
-              Ação/Aventura/Ficção Científica
+            <span className="text-xs font-extralight text-gray-400 flex gap-1">
+              {data?.genres.map((genre) => (
+                <Badge className="bg-[#485568] rounded-xs font-extralight">
+                  {genre.name}
+                </Badge>
+              ))}
             </span>
-            <p className="leading-tight text-white">
-              Princess Leia is captured and held hostage by the evil Imperial
-              forces in their effort to take over the galactic Empire.
-              Venturesome Luke Skywalker and dashing captain Han Solo team
-              together with the loveable robot duo R2-D2 and C-3PO to rescue the
-              beautiful princess and restore peace and justice in the Empire.
-            </p>
+            <p className="leading-tight text-white">{data?.overview}</p>
             <div className="flex flex-col gap-2">
               <h5 className="font-black uppercase text-white">Elenco</h5>
-              <div className="flex flex-wrap gap-1">
-                <Badge className="bg-[#485568] rounded-xs">Mark Hammill</Badge>
-                <Badge className="bg-[#485568] rounded-xs">Harrison Ford</Badge>
-                <Badge className="bg-[#485568] rounded-xs">
-                  James Earl Jones
-                </Badge>
-                <Badge className="bg-[#485568] rounded-xs">
-                  Carrie Fischer
-                </Badge>
-                <Badge className="bg-[#485568] rounded-xs">Alec Guiness</Badge>
-                <Badge className="bg-[#485568] rounded-xs">Peter Mayhew</Badge>
-                <Badge className="bg-[#485568] rounded-xs">David Prowse</Badge>
-                <Badge className="bg-[#485568] rounded-xs">Kenny Baker</Badge>
-                <Badge className="bg-[#485568] rounded-xs">
-                  Anthony Daniels
-                </Badge>
-                <Badge className="bg-[#485568] rounded-xs">Peter Cushing</Badge>
-              </div>
+              <CastList movieID={data?.id}></CastList>
             </div>
             <div className="flex flex-col gap-2">
               <h5 className="font-black uppercase text-white">Ações</h5>
