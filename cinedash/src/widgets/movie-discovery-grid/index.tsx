9ac1f5de7d041ff509ff027c07e85ca842817898
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-21 01:20:34 -0300

    feat: cria e implementa skeleton de lista horizontal de filmes

--- cinedash/src/widgets/movie-discovery-grid/index.tsx
+++ cinedash/src/widgets/movie-discovery-grid/index.tsx
@@ -1,100 +1,121 @@
-import {
-  ArrowLeftIcon,
-  ArrowRightIcon,
-  SlidersHorizontalIcon,
-} from "lucide-react";
-import { Input } from "../../components/ui/input";
-import { Toggle } from "../../components/ui/toggle";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../../components/ui/select";
-import MovieCard from "../../shared/ui/movie-card";
-import { Button } from "../../components/ui/button";
+import { useState } from "react"
+import { useDebounce } from "../../shared/lib/use-debounce"
+import { useFilteredMovies } from "../../entities/movie/api/use-filtered-movies"
+import { Input } from "../../components/ui/input"
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "../../components/ui/select"
+import { genres } from "../../shared/model/genres"
+import { ArrowLeftIcon, ArrowRightIcon, StarIcon } from "lucide-react"
+import { Button } from "../../components/ui/button"
+import MovieCard from "../../shared/ui/movie-card"
 
 const MoviedDiscoveryGrid = () => {
+  const [year, setYear] = useState<string>()
+  const [genre, setGenre] = useState<string>()
+  const [rating, setRating] = useState<string>()
+  const [title, setTitle] = useState("")
+
+  const debouncedTitle = useDebounce(title, 500)
+
+  const { data } = useFilteredMovies({
+    year,
+    genre,
+    rating,
+    title: debouncedTitle,
+  })
+
   return (
-    <>
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-col gap-4">
-          <div className="flex items-center gap-4 justify-between">
-            <Input
-              className="bg-white h-12 md:w-150"
-              type="text"
-              placeholder="Pesquise pelo nome do filme..."
-            ></Input>
-            {/* <Toggle>
-              <SlidersHorizontalIcon className="text-gray-500"></SlidersHorizontalIcon>
-              <span className="text-gray-400 hidden md:block">
-                Exibir filtros
-              </span>
-            </Toggle> */}
-          </div>
-          <div className="flex flex-col md:flex-row items-center gap-2 md:gap-4 p-2 md:p-4 border border-gray-400 rounded-sm">
-            <legend className="text-gray-400 text-sm">Filtrar por:</legend>
-            <div className="flex gap-2 md:gap-4">
-              <Select>
-                <SelectTrigger className="w-full max-w-48 bg-gray-300 h-12!">
-                  <SelectValue placeholder="GÊNERO" />
-                </SelectTrigger>
-                <SelectContent className="bg-gray-300">
-                  <SelectGroup>
-                    <SelectItem value="apple">Apple</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-              <Select>
-                <SelectTrigger className="w-full max-w-48 bg-gray-300 h-12!">
-                  <SelectValue placeholder="ANO" />
-                </SelectTrigger>
-                <SelectContent className="bg-gray-300">
-                  <SelectGroup>
-                    <SelectItem value="apple">Apple</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-              <Select>
-                <SelectTrigger className="w-full max-w-48 bg-gray-300 h-12!">
-                  <SelectValue placeholder="AVALIAÇÂO" />
-                </SelectTrigger>
-                <SelectContent className="bg-gray-300">
-                  <SelectGroup>
-                    <SelectItem value="apple">Apple</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-        </div>
-        <div className="grid grid-cols-3 md:grid-cols-6 gap-2 md:gap-4 w-full">
-          <MovieCard></MovieCard>
-          <MovieCard></MovieCard>
-          <MovieCard></MovieCard>
-          <MovieCard></MovieCard>
-          <MovieCard></MovieCard>
-          <MovieCard></MovieCard>
-          <MovieCard></MovieCard>
-          <MovieCard></MovieCard>
-          <MovieCard></MovieCard>
-          <MovieCard></MovieCard>
-          <MovieCard></MovieCard>
-          <MovieCard></MovieCard>
-        </div>
-        <div className="flex items-center justify-between">
-          <Button className="bg-gray-400 h-12 uppercase font-extralight text-gray-800">
-            <ArrowLeftIcon></ArrowLeftIcon> Página anterior
-          </Button>
-          <Button className="bg-gray-400 h-12 uppercase font-extralight text-gray-800">
-            Próxima página <ArrowRightIcon></ArrowRightIcon>
-          </Button>
-        </div>
+    <div className="flex flex-col gap-4">
+      {/* INPUT */}
+      <Input
+        className="bg-white h-12 md:w-150"
+        type="text"
+        placeholder="Pesquise pelo nome do filme..."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      {/* FILTROS */}
+      <div className="flex flex-col md:flex-row gap-2 md:gap-4 p-2 md:p-4 border border-gray-400 rounded-sm">
+        <legend className="text-gray-400 text-sm">
+          Filtrar por:
+        </legend>
+
+        <Select onValueChange={setGenre}>
+          <SelectTrigger className="w-full max-w-48 bg-gray-300 h-12!">
+            <SelectValue placeholder="GÊNERO" />
+          </SelectTrigger>
+
+          <SelectContent className="bg-gray-300">
+            <SelectGroup>
+              {genres.map((g) => (
+                <SelectItem key={g.id} value={String(g.id)}>
+                  {g.name}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Select onValueChange={setYear}>
+          <SelectTrigger className="w-full max-w-48 bg-gray-300 h-12!">
+            <SelectValue placeholder="ANO" />
+          </SelectTrigger>
+
+          <SelectContent className="bg-gray-300">
+            <SelectGroup>
+              {Array.from({ length: 2026 - 1950 + 1 }, (_, i) => {
+                const year = 2026 - i
+
+                return (
+                  <SelectItem key={year} value={String(year)}>
+                    {year}
+                  </SelectItem>
+                )
+              })}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Select onValueChange={setRating}>
+          <SelectTrigger className="w-full max-w-48 bg-gray-300 h-12!">
+            <SelectValue placeholder="AVALIAÇÃO" />
+          </SelectTrigger>
+
+          <SelectContent className="bg-gray-300">
+            <SelectGroup>
+              {Array.from({ length: 10 }, (_, i) => {
+                const value = i + 1
+
+                return (
+                  <SelectItem key={value} value={String(value)}>
+                    <StarIcon className="fill-foreground" />
+                    {value}
+                  </SelectItem>
+                )
+              })}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
-    </>
-  );
-};
+
+      {/* GRID */}
+      <div className="grid grid-cols-3 md:grid-cols-6 gap-2 md:gap-4 w-full">
+        {/* {data?.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))} */}
+      </div>
+
+      {/* PAGINAÇÃO */}
+      <div className="flex items-center justify-between">
+        <Button className="bg-gray-400 h-12 uppercase font-extralight text-gray-800">
+          <ArrowLeftIcon /> Página anterior
+        </Button>
+
+        <Button className="bg-gray-400 h-12 uppercase font-extralight text-gray-800">
+          Próxima página <ArrowRightIcon />
+        </Button>
+      </div>
+    </div>
+  )
+}
 
 export default MoviedDiscoveryGrid;
--- cinedash/src/widgets/movie-horizontal-list/index.tsx
+++ cinedash/src/widgets/movie-horizontal-list/index.tsx
@@ -2,6 +2,7 @@ import { usePopularMovies } from "../../entities/movie/api/use-popular-movies";
 import MovieCard from "../../shared/ui/movie-card";
 import type { IMovie } from "../../entities/movie/model/types";
 import { Link } from "@tanstack/react-router";
+import HorizontalListSkeleton from "./ui/skeleton";
 
 type Props = {
   title: string;
@@ -16,28 +17,27 @@ const MovieHorizontalList = ({
   showHyperlink,
   hyperlinkRoute,
 }: Props) => {
-
   const { data, isLoading } = usePopularMovies(displayMode === "popular");
 
   return (
     <>
       <div className="flex flex-col gap-2 text-white">
         <div className="flex justify-between items-center">
-          <h3 className="uppercase font-black text-xl md:text-2xl">{title}</h3>
+          <h3 className="uppercase font-black text-xl md:text-2xl inter-title">{title}</h3>
           {showHyperlink && (
-            <Link to={hyperlinkRoute} className="text-sm text-muted-foreground">
+            <Link to={hyperlinkRoute} className="text-sm text-grey-400">
               Ver mais...
             </Link>
           )}
         </div>
         <div className="flex gap-2 md:gap-4 overflow-x-scroll pb-4 h-60 md:h-80 w-full">
-          {isLoading ? (
-            <p>Carregando filmes...</p>
-          ) : (
-            data?.map((movie: IMovie) => (
-              <MovieCard movie={movie} key={movie.id}></MovieCard>
-            ))
-          )}
+          {isLoading
+            ? Array.from({ length: 8 }).map((_, i) => (
+                <HorizontalListSkeleton key={i} />
+              ))
+            : data?.map((movie: IMovie) => (
+                <MovieCard movie={movie} key={movie.id}></MovieCard>
+              ))}
         </div>
       </div>
     </>
