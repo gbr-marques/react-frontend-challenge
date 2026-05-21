4995137828180d7bb3b4a05ad7cb00c61345685c
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-21 13:55:21 -0300

    style: migra classe de aspect ratio da tag img para a tag que a contém, a fim de evitar inconsistencias e quebras de layout

--- cinedash/src/shared/ui/movie-card/index.tsx
+++ cinedash/src/shared/ui/movie-card/index.tsx
@@ -24,12 +24,12 @@ export function MovieCard({ movie }: Props) {
   return (
     <>
       <HoverCard openDelay={50} closeDelay={50}>
-        <HoverCardTrigger className="min-w-fit!">
+        <HoverCardTrigger className="min-w-fit! aspect-[65/98]!">
           <img
             onClick={() => navigate({ to: `/details/${movie?.id}` })}
             src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`}
             alt={`Pôster do filme ${movie?.title}`}
-            className="max-h-full aspect-[65/98]! rounded-sm border border-[#4D4D4D] shadow-lg"
+            className="max-h-full  rounded-sm border border-[#4D4D4D] shadow-lg"
           />
         </HoverCardTrigger>
         <HoverCardContent className="bg-[#14181ce0] text-white flex flex-col gap-2 items-center max-w-45 text-center -mt-62.5 backdrop-blur-sm">
