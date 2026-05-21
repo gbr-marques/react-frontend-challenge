744967c4562e27dd1e0998cb23c0fed8bcb0f13b
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-21 13:47:59 -0300

    feat: renderiza lista de filmes filtrados no grid de widget de filmes pesquisados

--- cinedash/src/widgets/movie-discovery-grid/index.tsx
+++ cinedash/src/widgets/movie-discovery-grid/index.tsx
@@ -1,44 +1,49 @@
-import { useState } from "react"
-import { useDebounce } from "../../shared/lib/use-debounce"
-import { useFilteredMovies } from "../../entities/movie/api/use-filtered-movies"
-import { Input } from "../../components/ui/input"
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "../../components/ui/select"
-import { genres } from "../../shared/model/genres"
-import { ArrowLeftIcon, ArrowRightIcon, StarIcon } from "lucide-react"
-import { Button } from "../../components/ui/button"
-import MovieCard from "../../shared/ui/movie-card"
+import { useState } from "react";
+import { useDebounce } from "../../shared/lib/use-debounce";
+import { useFilteredMovies } from "../../entities/movie/api/use-filtered-movies";
+import { Input } from "../../components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../../components/ui/select";
+import { genres } from "../../shared/model/genres";
+import { ArrowLeftIcon, ArrowRightIcon, StarIcon } from "lucide-react";
+import { Button } from "../../components/ui/button";
+import MovieCard from "../../shared/ui/movie-card";
+import MovieCardSkeleton from "../../shared/ui/movie-card/skeleton";
 
 const MoviedDiscoveryGrid = () => {
-  const [year, setYear] = useState<string>()
-  const [genre, setGenre] = useState<string>()
-  const [rating, setRating] = useState<string>()
-  const [title, setTitle] = useState("")
+  const [year, setYear] = useState<string>();
+  const [genre, setGenre] = useState<string>();
+  const [rating, setRating] = useState<string>();
+  const [title, setTitle] = useState("");
 
-  const debouncedTitle = useDebounce(title, 500)
+  const debouncedTitle = useDebounce(title, 500);
 
-  const { data } = useFilteredMovies({
+  const { data, isLoading, error } = useFilteredMovies({
     year,
     genre,
     rating,
-    title: debouncedTitle,
-  })
+  });
 
   return (
     <div className="flex flex-col gap-4">
       {/* INPUT */}
-      <Input
+      {/* <Input
         className="bg-white h-12 md:w-150"
         type="text"
         placeholder="Pesquise pelo nome do filme..."
         value={title}
         onChange={(e) => setTitle(e.target.value)}
-      />
+      /> */}
 
       {/* FILTROS */}
       <div className="flex flex-col md:flex-row gap-2 md:gap-4 p-2 md:p-4 border border-gray-400 rounded-sm">
-        <legend className="text-gray-400 text-sm">
-          Filtrar por:
-        </legend>
+        <legend className="text-gray-400 text-sm">Filtrar por:</legend>
 
         <Select onValueChange={setGenre}>
           <SelectTrigger className="w-full max-w-48 bg-gray-300 h-12!">
@@ -63,13 +68,13 @@ const MoviedDiscoveryGrid = () => {
           <SelectContent className="bg-gray-300">
             <SelectGroup>
               {Array.from({ length: 2026 - 1950 + 1 }, (_, i) => {
-                const year = 2026 - i
+                const year = 2026 - i;
 
                 return (
                   <SelectItem key={year} value={String(year)}>
                     {year}
                   </SelectItem>
-                )
+                );
               })}
             </SelectGroup>
           </SelectContent>
@@ -83,14 +88,14 @@ const MoviedDiscoveryGrid = () => {
           <SelectContent className="bg-gray-300">
             <SelectGroup>
               {Array.from({ length: 10 }, (_, i) => {
-                const value = i + 1
+                const value = i + 1;
 
                 return (
                   <SelectItem key={value} value={String(value)}>
                     <StarIcon className="fill-foreground" />
                     {value}
                   </SelectItem>
-                )
+                );
               })}
             </SelectGroup>
           </SelectContent>
@@ -98,10 +103,14 @@ const MoviedDiscoveryGrid = () => {
       </div>
 
       {/* GRID */}
-      <div className="grid grid-cols-3 md:grid-cols-6 gap-2 md:gap-4 w-full">
-        {/* {data?.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))} */}
+      <div className="grid grid-cols-4 md:grid-cols-6 gap-2 md:gap-4 w-full">
+        {isLoading
+          ? Array.from({ length: 20 }).map((_, i) => (
+              <MovieCardSkeleton></MovieCardSkeleton>
+            ))
+          : data.results?.map((movie: IMovie) => (
+              <MovieCard key={movie.id} movie={movie} />
+            ))}
       </div>
 
       {/* PAGINAÇÃO */}
@@ -115,7 +124,7 @@ const MoviedDiscoveryGrid = () => {
         </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default MoviedDiscoveryGrid;
