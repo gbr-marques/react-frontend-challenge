8ddb36343d4a9f395aa2a38871d34462d356ab41
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-21 13:38:36 -0300

    refactor: exclui skeleton do widget de lista horizontal e substitui pelo skeleton do componente de ui moviecard

--- cinedash/src/widgets/movie-horizontal-list/index.tsx
+++ cinedash/src/widgets/movie-horizontal-list/index.tsx
@@ -2,7 +2,7 @@ import { usePopularMovies } from "../../entities/movie/api/use-popular-movies";
 import MovieCard from "../../shared/ui/movie-card";
 import type { IMovie } from "../../entities/movie/model/types";
 import { Link } from "@tanstack/react-router";
-import HorizontalListSkeleton from "./ui/skeleton";
+import MovieCardSkeleton from "../../shared/ui/movie-card/skeleton";
 
 type Props = {
   title: string;
@@ -33,7 +33,7 @@ const MovieHorizontalList = ({
         <div className="flex gap-2 md:gap-4 overflow-x-scroll pb-4 h-60 md:h-80 w-full">
           {isLoading
             ? Array.from({ length: 8 }).map((_, i) => (
-                <HorizontalListSkeleton key={i} />
+                <MovieCardSkeleton key={i} />
               ))
             : data?.results.map((movie: IMovie) => (
                 <MovieCard movie={movie} key={movie.id}></MovieCard>
--- cinedash/src/widgets/movie-horizontal-list/ui/skeleton.tsx
+++
@@ -1,9 +0,0 @@
-import { Skeleton } from "../../../components/ui/skeleton";
-
-function HorizontalListSkeleton() {
-  return <>
-    <Skeleton className="aspect-[65/98]! h-full bg-[#2b363f]"></Skeleton>
-  </>
-}
-
-export default HorizontalListSkeleton;
