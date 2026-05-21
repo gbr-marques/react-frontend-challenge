d96496c17013ba581804a7e2ed4ba568d329f07b
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-21 13:36:52 -0300

    chore: desabilita botões de navegação de grid de filmes populares durante fetch de filmes

--- cinedash/src/widgets/popular-movies-grid/ui/popular-movies-grid.tsx
+++ cinedash/src/widgets/popular-movies-grid/ui/popular-movies-grid.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeftIcon, ArrowRightIcon } from "lucide-react";
+import { ArrowLeftIcon, ArrowRightIcon, LoaderCircleIcon } from "lucide-react";
 import { Button } from "../../../components/ui/button";
 import { usePopularMovies } from "../../../entities/movie/api/use-popular-movies";
 import type { IMovie } from "../../../entities/movie/model/types";
@@ -29,18 +29,28 @@ export function PopularMoviesGrid() {
         <div className="flex items-center justify-between">
           <Button
             className="bg-gray-400 h-12 uppercase font-extralight text-gray-800"
-            disabled={page == 1}
+            disabled={page == 1 || isLoading}
             onClick={() => setPage((currtentPage) => (currtentPage -= 1))}
           >
-            <ArrowLeftIcon /> Página anterior
+            {isLoading ? (
+              <LoaderCircleIcon className="animate-spin"></LoaderCircleIcon>
+            ) : (
+              <ArrowRightIcon />
+            )}{" "}
+            Página anterior
           </Button>
           {page}
           <Button
             className="bg-gray-400 h-12 uppercase font-extralight text-gray-800"
-            disabled={page == data?.total_pages}
+            disabled={page == data?.total_pages || isLoading}
             onClick={() => setPage((currtentPage) => (currtentPage += 1))}
           >
-            Próxima página <ArrowRightIcon />
+            Próxima página
+            {isLoading ? (
+              <LoaderCircleIcon className="animate-spin"></LoaderCircleIcon>
+            ) : (
+              <ArrowRightIcon />
+            )}
           </Button>
         </div>
       </div>
