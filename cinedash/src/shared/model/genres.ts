12efc5f25ca32d4eaf8db14c44755807720570b4
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-21 01:24:47 -0300

    chore: cria lista estática de generos de filmes

---
+++ cinedash/src/shared/model/genres.ts
@@ -0,0 +1,23 @@
+import type { IGenre } from "../../entities/movie/model/types";
+
+export const genres: IGenre[] = [
+  { id: 28, name: 'Ação' },
+  { id: 12, name: 'Aventura' },
+  { id: 16, name: 'Animação' },
+  { id: 35, name: 'Comédia' },
+  { id: 80, name: 'Crime' },
+  { id: 99, name: 'Documentário' },
+  { id: 18, name: 'Drama' },
+  { id: 10751, name: 'Família' },
+  { id: 14, name: 'Fantasia' },
+  { id: 36, name: 'História' },
+  { id: 27, name: 'Terror' },
+  { id: 10402, name: 'Música' },
+  { id: 9648, name: 'Mistério' },
+  { id: 10749, name: 'Romance' },
+  { id: 878, name: 'Ficção científica' },
+  { id: 10770, name: 'Filme para TV' },
+  { id: 53, name: 'Suspense' },
+  { id: 10752, name: 'Guerra' },
+  { id: 37, name: 'Faroeste' },
+]
