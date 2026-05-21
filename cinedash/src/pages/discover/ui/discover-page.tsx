208e36dc4ac3d3bee99a0447c310bbe28550e504
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-21 19:14:48 -0300

    style: refina estilos e otimiza layout para melhor responsividade

--- cinedash/src/pages/discover/ui/discover-page.tsx
+++ cinedash/src/pages/discover/ui/discover-page.tsx
@@ -13,30 +13,34 @@ import { PopularMoviesGrid } from "../../../widgets/popular-movies-grid/ui/popul
 export function DiscoverPage() {
   return (
     <>
-      <section className="min-h-[75dvh] bg-[#1D242A] p-4 py-8 flex flex-col gap-8">
-        <div className="text-white text-center leading-tight flex flex-col items-center gap-4 ">
-          <h1 className="text-2xl md:text-4xl">Navegue por novas histórias </h1>
-          <p className="md:max-w-200 lg:max-w-250">
-            Descubra produções populares, acompanhe novidades e avalie títulos
-            para compor o catálogo ideal.
-          </p>
+      <section className="min-h-[75dvh] bg-[#1D242A] p-4 py-8 flex flex-col items-center gap-8">
+        <div className="w-full max-w-[1350px] flex flex-col gap-8">
+          <div className="text-white text-center leading-tight flex flex-col items-center gap-4 ">
+            <h1 className="text-2xl md:text-4xl georgia font-bold">
+              Navegue por novas histórias
+            </h1>
+            <p className="md:max-w-200 lg:max-w-250">
+              Descubra produções populares, acompanhe novidades e avalie títulos
+              para compor o catálogo ideal.
+            </p>
+          </div>
+          <Tabs defaultValue="popular" className="flex flex-col gap-4">
+            <TabsList className="bg-gray-300! w-full! md:w-fit! md:flex md:gap-2">
+              <TabsTrigger value="popular">Os mais populares</TabsTrigger>
+              <TabsTrigger value="search">Buscar por título</TabsTrigger>
+              <TabsTrigger value="filters">Filtros avançados</TabsTrigger>
+            </TabsList>
+            <TabsContent value="popular">
+              <PopularMoviesGrid></PopularMoviesGrid>
+            </TabsContent>
+            <TabsContent value="search">
+              <MovieSearchGrid></MovieSearchGrid>
+            </TabsContent>
+            <TabsContent value="filters">
+              <MoviedDiscoveryGrid></MoviedDiscoveryGrid>
+            </TabsContent>
+          </Tabs>
         </div>
-        <Tabs defaultValue="popular" className="flex flex-col gap-4">
-          <TabsList className="bg-gray-300! w-full! md:w-fit! md:flex md:gap-2">
-            <TabsTrigger value="popular">Os mais populares</TabsTrigger>
-            <TabsTrigger value="search">Buscar por título</TabsTrigger>
-            <TabsTrigger value="filters">Filtros avançados</TabsTrigger>
-          </TabsList>
-          <TabsContent value="popular">
-            <PopularMoviesGrid></PopularMoviesGrid>
-          </TabsContent>
-          <TabsContent value="search">
-            <MovieSearchGrid></MovieSearchGrid>
-          </TabsContent>
-          <TabsContent value="filters">
-            <MoviedDiscoveryGrid></MoviedDiscoveryGrid>
-          </TabsContent>
-        </Tabs>
       </section>
     </>
   );
