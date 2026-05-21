e1e60f13f14e18b9d39c24c1b45bc3dac7d63fbe
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-21 14:25:59 -0300

    style: modifica classe de altura de img de movie card para evitar inconsistências de layout

--- cinedash/src/shared/ui/movie-card/index.tsx
+++ cinedash/src/shared/ui/movie-card/index.tsx
@@ -29,7 +29,7 @@ export function MovieCard({ movie }: Props) {
             onClick={() => navigate({ to: `/details/${movie?.id}` })}
             src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`}
             alt={`Pôster do filme ${movie?.title}`}
-            className="max-h-full  rounded-sm border border-[#4D4D4D] shadow-lg"
+            className="h-full rounded-sm border border-[#4D4D4D] shadow-lg text-white text-xs leading-tight text-center"
           />
         </HoverCardTrigger>
         <HoverCardContent className="bg-[#14181ce0] text-white flex flex-col gap-2 items-center max-w-45 text-center -mt-62.5 backdrop-blur-sm">
