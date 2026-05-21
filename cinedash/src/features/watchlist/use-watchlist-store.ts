d3974ab9f0c477a14a8fdf42f61e74e79792c1fb
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-21 16:19:52 -0300

    chore: ajusta tipagem de store de watchlist

--- cinedash/src/features/watchlist/use-watchlist-store.ts
+++ cinedash/src/features/watchlist/use-watchlist-store.ts
@@ -7,24 +7,27 @@ type IWatchlistStore = {
 
   addMovie: (movie: IMovie) => void;
 
-  removeMovie: (movieID: number) => void;
+  removeMovie: (movieID: number | undefined) => void;
 
-  isFavorite: (movieID: number | undefined) => void;
+  isFavorite: (movieID: number | undefined) => boolean;
 };
-
 export const useWatchlistStore = create<IWatchlistStore>()(
   persist(
     (set, get) => ({
       watchlist: [],
 
       addMovie: (movie) =>
-        set((state) => ({
-          watchlist: [...state.watchlist, movie],
-        })),
+        set((state) => {
+          console.log(state.watchlist);
+
+          return {
+            watchlist: [...state.watchlist, movie],
+          };
+        }),
 
       removeMovie: (movieID) =>
         set((state) => ({
-          watchlist: state.watchlist.filter((movie) => movie.id === movieID),
+          watchlist: state.watchlist.filter((movie) => movie.id !== movieID),
         })),
 
       isFavorite: (movieID) =>
