91db9a7683ee13b7c89677cdc0e6603717b2fca2
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-20 23:34:33 -0300

    chore: adiciona tipagem às informações de login

--- cinedash/src/features/auth/api/use-login.ts
+++ cinedash/src/features/auth/api/use-login.ts
@@ -1,12 +1,13 @@
 import { useMutation } from "@tanstack/react-query";
+import type { ILogin } from "../model/types";
 
-export function useAuthentication(formData) {
+export function useAuthentication(formData: ILogin) {
   return useMutation({
     mutationFn: () => logUser(formData),
   });
 }
 
-async function logUser(formData) {
+async function logUser(formData: ILogin) {
   const sessionToken = crypto.randomUUID();
   localStorage.setItem("token", sessionToken);
 }
