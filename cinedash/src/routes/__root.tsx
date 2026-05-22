297bb8684f3b327a999b842c76ff3a1ed1cd26c9
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-21 22:28:12 -0300

    chore: oculta cabeçalho e rodapé pra usuários não autenticados

--- cinedash/src/routes/__root.tsx
+++ cinedash/src/routes/__root.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Outlet, createRootRoute, useRouterState } from "@tanstack/react-router";
 import Header from "../widgets/header";
 import Footer from "../widgets/footer";
+import { useAuthStore } from "../stores/auth/use-auth-store";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -13,11 +14,14 @@ function RootComponent() {
   const pathname = useRouterState({
   select: (state) => state.location.pathname,
 })
+
+  const { isAuthenticated } = useAuthStore()
+
   return (
     <React.Fragment>
-      {pathname !== '/' && <Header />}
+      {isAuthenticated && <Header />}
       <Outlet />
-      {pathname !== '/' && <Footer />}
+      {isAuthenticated && <Footer />}
     </React.Fragment>
   );
 }
