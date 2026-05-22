2032165a676f63d215927506c52a1a72f89731b4
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-21 22:40:08 -0300

    chore: instala deps e implementa toasts (sonner)

--- cinedash/src/app/App.tsx
+++ cinedash/src/app/App.tsx
@@ -1,16 +1,17 @@
-import { createRouter, RouterProvider } from '@tanstack/react-router'
-import { useState } from 'react'
-import { routeTree } from '../routeTree.gen'
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { useState } from "react";
+import { routeTree } from "../routeTree.gen";
+import { Toaster } from "../components/ui/sonner";
 
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 
 function App() {
-
   return (
     <>
+      <Toaster />
       <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
