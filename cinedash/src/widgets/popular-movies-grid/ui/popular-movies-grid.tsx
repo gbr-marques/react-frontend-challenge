f96b54de7bb0664d2ff9eff1d269c38afa90a2d8
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-21 21:24:13 -0300

    fix: corrige mensagem de erro ao buscar filmes populares

--- cinedash/src/widgets/popular-movies-grid/ui/popular-movies-grid.tsx
+++ cinedash/src/widgets/popular-movies-grid/ui/popular-movies-grid.tsx
@@ -10,7 +10,7 @@ import { MovieGrid } from "../../movie-grid";
 export function PopularMoviesGrid() {
   const [page, setPage] = useState<number>(1);
 
-  const { data, isLoading } = usePopularMovies(true, page);
+  const { data, isLoading, error, refetch } = usePopularMovies(true, page);
 
   return (
     <>
@@ -18,14 +18,38 @@ export function PopularMoviesGrid() {
         <h3 className="uppercase font-black text-xl text-white md:text-2xl inter-title">
           Filmes populares
         </h3>
-        
-        <MovieGrid
-          movies={data?.results}
-          isLoading={isLoading}
-          page={page}
-          totalPages={data?.total_pages}
-          setPage={setPage}
-        />
+
+        {data?.total_results === 0 ? (
+          <div className="flex h-150 w-full flex-col items-center justify-center gap-2 p-4 text-center leading-tight text-white md:gap-4">
+            <h3 className="text-xl font-bold md:text-2xl">
+              Nenhum filme encontrado
+            </h3>
+
+            <p>Não encontramos resultados para os filtros escolhidos.</p>
+          </div>
+        ) : error ? (
+          <div className="flex h-150 w-full flex-col items-center justify-center gap-2 p-4 text-center leading-tight text-white md:gap-4">
+            <h3 className="text-xl font-bold md:text-2xl">Ops...</h3>
+
+            <p>Ocorreu um erro ao buscar os filmes.</p>
+
+            <Button
+              onClick={() => refetch()}
+              variant="secondary"
+              className="h-12"
+            >
+              Tentar novamente
+            </Button>
+          </div>
+        ) : (
+          <MovieGrid
+            movies={data?.results}
+            isLoading={isLoading}
+            page={page}
+            totalPages={data?.total_pages}
+            setPage={setPage}
+          />
+        )}
       </div>
     </>
   );
