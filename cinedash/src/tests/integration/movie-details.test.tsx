0f9ef2c40c65cbb496cc0a9641b6c594d9511a5f
gbr_marques <gabrielmarques@prefeitura.sp.gov.br>
2026-05-22 01:18:29 -0300

    chore: iguala filme mockado no teste ao filme recebido da api

--- cinedash/src/tests/integration/movie-details.test.tsx
+++ cinedash/src/tests/integration/movie-details.test.tsx
@@ -8,25 +8,69 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const movieMock = {
   adult: false,
   backdrop_path: "/qO55CD8tgVL1T4WKn6zYFFiD6lL.jpg",
+  belongs_to_collection: null,
+  budget: 0,
   genres: [
     {
       id: 28,
-      name: "Action",
+      name: "Ação",
+    },
+    {
+      id: 18,
+      name: "Drama",
+    },
+    {
+      id: 80,
+      name: "Crime",
     },
   ],
+  homepage: "",
   id: 1439930,
-  title: "The Punisher: One Last Kill",
+  imdb_id: "tt36042156",
+  origin_country: ["US"],
   original_language: "en",
   original_title: "The Punisher: One Last Kill",
   overview:
-    "As Frank Castle searches for meaning beyond revenge, an unexpected force pulls him back into the fight.",
+    "Enquanto Frank Castle busca um significado além da vingança, uma força inesperada o arrasta de volta para a luta.",
   popularity: 580.0449,
-  poster_path: "/qQclTgLMDvGBuUBFGHRipxkEwWR.jpg",
+  poster_path: "/ppRmgI2MbEOADvKKvlNoRJHPXOT.jpg",
+  production_companies: [
+    {
+      id: 420,
+      logo_path: "/hUzeosd33nzE5MCNsZxCGEKTXaQ.png",
+      name: "Marvel Studios",
+      origin_country: "US",
+    },
+    {
+      id: 176762,
+      logo_path: null,
+      name: "Kevin Feige Productions",
+      origin_country: "US",
+    },
+  ],
+  production_countries: [
+    {
+      iso_3166_1: "US",
+      name: "United States of America",
+    },
+  ],
   release_date: "2026-05-12",
-  tagline: "One last mission.",
+  revenue: 0,
+  runtime: 51,
+  softcore: false,
+  spoken_languages: [
+    {
+      english_name: "English",
+      iso_639_1: "en",
+      name: "English",
+    },
+  ],
+  status: "Released",
+  tagline: "Para encontrar a paz, ele deve perdê-la.",
+  title: "O Justiceiro: Uma Última Morte",
   video: false,
-  vote_average: 8.504,
-  vote_count: 1008,
+  vote_average: 8.501,
+  vote_count: 1004,
 };
 
 vi.mock("@tanstack/react-router", async () => {
@@ -66,12 +110,17 @@ describe("MovieDetailsPage", () => {
       </QueryClientProvider>,
     );
 
-    const button = screen.getByRole("button", {
-      name: /adicionar aos favoritos/i,
-    });
+    const button = await screen.findByText(/adicionar aos favoritos/i);
 
     await user.click(button);
 
-    expect(useWatchlistStore.getState().watchlist).toContainEqual(movieMock);
+    expect(useWatchlistStore.getState().watchlist).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: movieMock.id,
+          title: movieMock.title,
+        }),
+      ]),
+    );
   });
 });
